956aeb1049a1c8a5de6f5facc48f0bb6dcd2685d
William Basseto Sena <williamsena13@gmail.com>
2023-07-10 09:01:04 -0300

    ajuste seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(PaymentOptionSeeder::class);
+        $this->call(PaymentStatusSeeder::class);
+        $this->call(TransactionStatusSeeder::class);
     }
 }
